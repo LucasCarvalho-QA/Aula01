8e57ff4717e429e5421afcbcef430cf0ad079067
LucasCarvalho-QA <locarvalho.lucas@gmail.com>
2022-04-19 10:44:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string usuario = "";
+            string senha = "";
+            int idade = 0;
+
+            Console.WriteLine("Insira o seu usuário");
+            usuario = Console.ReadLine();
+
+            Console.WriteLine("Insira a sua senha");
+            senha = Console.ReadLine();
+
+            if (usuario == "lucas@auditeste.com.br" || senha == "123456")
+            {
+                Console.WriteLine("Usuário autenticado");
+            }
+            else
+            {
+                Console.WriteLine("Usuário ou senha incorretas");
+            }
+        }
+    }
+}
